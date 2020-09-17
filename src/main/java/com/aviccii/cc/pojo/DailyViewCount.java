4096a3a1c7ddc882b5e91fa33669e4816a70d80f
AVICCII <863134471@qq.com>
2020-09-17 14:28:00 +0800

    2020/09/17

--- src/main/java/com/aviccii/cc/pojo/DailyViewCount.java
+++ src/main/java/com/aviccii/cc/pojo/DailyViewCount.java
@@ -1,20 +1,21 @@
 package com.aviccii.cc.pojo;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Id;
 
 @Entity
-@Table(name = "tb_kpi_daily")
-public class KpiDaily {
+@Table(name = "tb_daily_view_count")
+public class DailyViewCount {
 
   @Id
   private String id;
   	@Column(name = "view_count")
   private long view_count;
+  	@Column(name = "create_time")
+  private String create_time;
   	@Column(name = "update_time")
-  private java.sql.Timestamp update_time;
+  private String update_time;
 
 
   public String getId() {
@@ -35,11 +36,20 @@ public class KpiDaily {
   }
 
 
-  public java.sql.Timestamp getUpdate_time() {
+  public String getCreate_time() {
+    return create_time;
+  }
+
+  public void setCreate_time(String create_time) {
+    this.create_time = create_time;
+  }
+
+
+  public String getUpdate_time() {
     return update_time;
   }
 
-  public void setUpdate_time(java.sql.Timestamp update_time) {
+  public void setUpdate_time(String update_time) {
     this.update_time = update_time;
   }
 
